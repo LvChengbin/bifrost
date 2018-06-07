35c08a1b9c548ba4b1c61989410ad4608f6b94d5
LvChengbin <lvchengbin@nextseason.cc>
2018-06-07 18:49:26 +0800

    upgrade

---
+++ index.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const yargs = require( 'yargs' );
+const Liftoff = require( 'liftoff' );
+const v8flags = require( 'v8flags' );
+const console = require( './lib/shared/console' );
+const modules = require( './lib/modules' );
+
+/**
+ * to get the cli options.
+ */
+const options = yargs.argv;
+
+const cli = new Liftoff( {
+    name : 'bifrost',
+    processTitle : 'bifrost',
+    moduleName : 'bifrost',
+    configFiles : {
+        '.bifrost' : {
+            home : {
+                path : '~',
+                extensions : {
+                    '.js' : null,
+                    '.json' : null
+                },
+            },
+            cwd : {
+                path : '.',
+                extensions : {
+                    '.js' : null,
+                    '.json' : null
+                }
+            }
+        }
+    },
+    v8flags
+} );
+
+cli.on( 'require', name => {
+    console.log( `requiring external module: ${name}.` );
+} );
+
+cli.on( 'requireFail', name => {
+    console.warn( `failed to load external module: ${name}.` );
+} );
+
+cli.on( 'respawn', ( flags, child ) => {
+    console.info( `node flags detected: [ ${flags.join( ', ' )} ].` );
+    console.info( `respawned to PID: ${child.pid}.` );
+} );
+
+module.exports = () => {
+    cli.launch( {
+        cwd : options.cwd,
+        configPath : options.config
+    }, env => {
+        // bifrost send file.txt s95:~/ 
+        // bifrost get s95:~/file.txt ~/path
+        //console.debug( 'env', env );
+        modules( options );
+    } );
+};
