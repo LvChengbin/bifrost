93c683d0eb969bd6d516c0d7d3d5379a5d47f7c5
LvChengbin <lvchengbin@nextseason.cc>
2018-06-08 22:14:09 +0800

    upgrade

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 'use strict';
-
 const yargs = require( 'yargs' );
 const Liftoff = require( 'liftoff' );
 const v8flags = require( 'v8flags' );
 const console = require( './lib/shared/console' );
 const modules = require( './lib/modules' );
 const init = require( './lib/init' );
+const Config = require( '@lvchengbin/Config' );
+const is = require( '@lvchengbin/is' );
 
 /**
  * to get the cli options.
@@ -56,9 +57,16 @@ module.exports = () => {
         configPath : options.config
     }, env => {
         init();
-        // bifrost send file.txt s95:~/ 
-        // bifrost get s95:~/file.txt ~/path
-        //console.debug( 'env', env );
-        modules( options );
+        const configFiles = env.configFiles[ '.bifrost' ];
+        const configFile = env.configPath || configFiles.cwd || configFiles.home || null;
+        let config = {};
+
+        if( configFile ) {
+            config = require( configFile );
+            if( is.function( config ) ) {
+                config = config();
+            }
+        }
+        modules( options, new Config( config ) );
     } );
 };
